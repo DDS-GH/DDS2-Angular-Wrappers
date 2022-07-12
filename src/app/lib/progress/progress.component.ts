5d7aee130515cf3346e03ef15b7bc2404523e827
DDS-GH <100168488+DDS-GH@users.noreply.github.com>
2022-07-11 20:26:28 -0500

    fix/centering progress dot

--- src/app/lib/progress/progress.component.ts
+++ src/app/lib/progress/progress.component.ts
@@ -1,45 +1,67 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { setElementId } from "../helpers/dds.helpers";
+import { AfterViewInit, Component, Input, OnInit } from '@angular/core';
+import { setElementId } from '../helpers/dds.helpers';
 
 @Component({
   selector: `dds-progress`,
-  templateUrl: `./progress.component.html`
+  templateUrl: `./progress.component.html`,
 })
-export class ProgressComponent implements OnInit {
+export class ProgressComponent implements OnInit, AfterViewInit {
   @Input() elementId: string = ``;
   @Input() classList: string = ``;
   @Input() steps: any = [
     {
-      name: "Step Name",
-      state: "complete",
-      summary: "Summary text",
-      link: "http://www.example.com/"
+      name: 'Step Name',
+      state: 'complete',
+      summary: 'Summary text',
+      link: 'http://www.example.com/',
     },
     {
-      name: "Step Name",
-      state: "complete",
-      summary: "Summary text",
-      link: "http://www.example.com/"
+      name: 'Step Name',
+      state: 'complete',
+      summary: 'Summary text',
+      link: 'http://www.example.com/',
     },
     {
-      name: "Step Name",
-      state: "in-progress",
-      summary: "Summary text"
+      name: 'Step Name',
+      state: 'in-progress',
+      summary: 'Summary text',
     },
     {
-      name: "Step Name",
-      state: "inactive",
-      summary: "Summary text"
+      name: 'Step Name',
+      state: 'inactive',
+      summary: 'Summary text',
     },
     {
-      name: "Step Name",
-      state: "active",
-      summary: "Summary text",
-      link: "http://www.example.com/"
-    }
+      name: 'Step Name',
+      state: 'active',
+      summary: 'Summary text',
+      link: 'http://www.example.com/',
+    },
   ];
 
   ngOnInit() {
     this.elementId = setElementId(this.elementId);
   }
+
+  ngAfterViewInit() {
+    // This is a strange hack because the SCSS variable from DDS is not rendered properly
+    const rootEl = document.getElementById(this.elementId);
+    if (rootEl) {
+      // @ts-ignore
+      const currentEl: HTMLElement = rootEl.querySelector(`.dds__progress-tracker--in-progress .dds__progress-tracker__icon`);
+      // @ts-ignore
+      const parentEl: HTMLElement = currentEl.parentElement;
+      const parentBox = parentEl?.getBoundingClientRect();
+      if (currentEl && currentEl.id !== this.elementId && parentBox) {
+        const topStyle = currentEl.getAttribute(`style`);
+        if (!topStyle) {
+          const border = 2.5/2; // parentEl.style.borderWidth;
+          currentEl.style.top = `calc(50% - ${(parentBox.height / 4)+border}px)`;
+          parentEl.style.display = `flex`;
+          parentEl.style.justifyContent = `center`;
+        }
+      }
+    }
+  }
 }
+
--- src/app/pages/progress/progress.page.ts
+++ src/app/pages/progress/progress.page.ts
@@ -3,4 +3,35 @@ import { Component } from "@angular/core";
 @Component({
   templateUrl: "./progress.page.html"
 })
-export class ProgressPageComponent {}
+export class ProgressPageComponent {
+    public mySteps = [
+        {
+          name: 'Meow Meow',
+          state: 'complete',
+          summary: 'Mrrrr meow',
+          link: 'http://www.example.com/',
+        },
+        {
+          name: 'sandpaper tongue',
+          state: 'complete',
+          summary: 'Mrrrr meow',
+          link: 'http://www.example.com/',
+        },
+        {
+          name: 'meow miao',
+          state: 'in-progress',
+          summary: 'Mrrrr meow',
+        },
+        {
+          name: 'Laser mice',
+          state: 'inactive',
+          summary: 'Mrrrr meow',
+        },
+        {
+          name: 'rubbing belly',
+          state: 'active',
+          summary: 'Mrrrr meow',
+          link: 'http://www.example.com/',
+        },
+      ];
+}
