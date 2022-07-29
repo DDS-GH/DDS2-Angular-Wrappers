1eb7de77844eb49abe0e2315486e164cf2430dfb
DDS-GH <100168488+DDS-GH@users.noreply.github.com>
2022-07-29 08:35:21 -0500

    fix: adding closeOnClick to SideNav JIRA#DDSTM-1882;

--- src/app/lib/sidenav/sidenav.component.ts
+++ src/app/lib/sidenav/sidenav.component.ts
@@ -1,22 +1,38 @@
-import { Component, Input } from "@angular/core";
-import { DdsComponent } from "../helpers/dds.component";
-import { stringToBoolean, debounce, ddsIcon } from "../helpers/dds.helpers";
-import { handleResize } from "../helpers/mediasize";
+import { Component, Input } from '@angular/core';
+import { DdsComponent } from '../helpers/dds.component';
+import { stringToBoolean, ddsIcon } from '../helpers/dds.helpers';
 
 const icons: any = {
   left: ddsIcon(`chevron-left`),
-  right: ddsIcon(`chevron-right`)
+  right: ddsIcon(`chevron-right`),
 };
 
 @Component({
   selector: `dds-sidenav`,
   templateUrl: `./sidenav.component.html`,
-  styleUrls: [`./sidenav.component.scss`]
+  styleUrls: [`./sidenav.component.scss`],
 })
 export class SidenavComponent extends DdsComponent {
+  @Input() closeOnClick: any = false;
+
   // @ts-ignore
   ngOnInit() {
     super.ngOnInit();
     this.ddsInitializer = `SideNav`;
+    this.closeOnClick = stringToBoolean(this.closeOnClick);
+  }
+
+  // @ts-ignore
+  ngAfterViewInit(): void {
+    super.ngAfterViewInit();
+    if (this.closeOnClick) {
+      this.ddsElement
+        .querySelectorAll(`.dds__side-nav__item`)
+        .forEach((el: any) => {
+          el.addEventListener(`click`, () => {
+            this.ddsElement.SideNav.collapse();
+          });
+        });
+    }
   }
 }
